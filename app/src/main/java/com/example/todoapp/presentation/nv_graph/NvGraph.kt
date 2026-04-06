d8781e1df69a4e53db3b077d5f8bddf4ff549363
ivanpahlevi8 <83549388+ivanpahlevi8@users.noreply.github.com>
2026-04-06 07:24:45 +0700

    feat: Update loading state for boarding user

--- app/src/main/java/com/example/todoapp/presentation/nv_graph/NvGraph.kt
+++ app/src/main/java/com/example/todoapp/presentation/nv_graph/NvGraph.kt
@@ -1,10 +1,14 @@
 package com.example.todoapp.presentation.nv_graph
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.todoapp.presentation.main_navigation.MainNavigation
+import com.example.todoapp.presentation.on_board_screen.OnBoardScreen
+import com.example.todoapp.presentation.on_board_screen.OnBoardScreenState
+import com.example.todoapp.presentation.on_board_screen.OnBoardScreenViewModel
 
 @Composable
 fun NvGraph(route : String) {
@@ -25,7 +29,16 @@ fun NvGraph(route : String) {
         composable(
             route = Routes.OnBoardScreenRoutes.route
         ) {
+            val onBoardScreenViewModel : OnBoardScreenViewModel = hiltViewModel()
 
+            OnBoardScreen(
+                onEvent = {
+                    event -> onBoardScreenViewModel.onEvent(
+                        event
+                    )
+                },
+                state = onBoardScreenViewModel.onBoardState.value
+            )
         }
     }
 }
--- app/src/main/java/com/example/todoapp/presentation/on_board_screen/OnBoardScreen.kt
+++ app/src/main/java/com/example/todoapp/presentation/on_board_screen/OnBoardScreen.kt
@@ -2,15 +2,29 @@ package com.example.todoapp.presentation.on_board_screen
 
 import android.content.res.Configuration
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import com.example.todoapp.R
 import com.example.todoapp.core.value.Dimension
 import com.example.todoapp.presentation.on_board_screen.component.OnBoardData
 import com.example.todoapp.presentation.on_board_screen.component.OnBoardItem
@@ -21,41 +35,96 @@ import kotlinx.coroutines.launch
 @Composable
 fun OnBoardScreen(
     onEvent : (OnBoardScreenEvent) -> Unit,
+    state : OnBoardScreenState,
 ){
     val coroutineScope = rememberCoroutineScope()
     val pagerState = rememberPagerState(initialPage = 0) { onBoardScreenData.size }
 
-    HorizontalPager(
-        state = pagerState
-    ) {
-        index: Int ->
-        val getData : OnBoardData = onBoardScreenData[index]
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier
+                .fillMaxSize(),
+        ) {
+                index: Int ->
+            val getData : OnBoardData = onBoardScreenData[index]
 
-        OnBoardItem(
-            item = getData,
-            showBack = (index > 0),
-            showNext = (index == 0 || index < onBoardScreenData.size - 1),
-            showFinish = (index == onBoardScreenData.size - 1),
-            onNext = {
-                coroutineScope.launch {
-                    pagerState.animateScrollToPage(
-                        page = pagerState.currentPage + 1
+            OnBoardItem(
+                item = getData,
+                showBack = (index > 0),
+                showNext = (index == 0 || index < onBoardScreenData.size - 1),
+                showFinish = (index == onBoardScreenData.size - 1),
+                onNext = {
+                    coroutineScope.launch {
+                        pagerState.animateScrollToPage(
+                            page = pagerState.currentPage + 1
+                        )
+                    }
+                },
+                onBack = {
+                    coroutineScope.launch {
+                        pagerState.animateScrollToPage(
+                            page = pagerState.currentPage - 1
+                        )
+                    }
+                },
+                onFinish = {
+                    onEvent(
+                        OnBoardScreenEvent.BoardEvent
                     )
                 }
-            },
-            onBack = {
-                coroutineScope.launch {
-                    pagerState.animateScrollToPage(
-                        page = pagerState.currentPage - 1
-                    )
+            )
+        }
+
+        when(state) {
+            is OnBoardScreenState.LoadingState -> {
+                Dialog(
+                    onDismissRequest = {}
+                ) {
+                    Surface(
+                        shape = MaterialTheme.shapes.medium,
+                        color = colorResource(
+                            id = R.color.card_background_color2
+                        ),
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .padding(
+                                    vertical = Dimension.MEDIUM_PADDING1,
+                                    horizontal = Dimension.MEDIUM_PADDING3
+                                ),
+                            verticalArrangement = Arrangement.Top,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            CircularProgressIndicator()
+                            Spacer(
+                                modifier = Modifier
+                                    .height(
+                                        Dimension.MEDIUM_PADDING2
+                                    )
+                            )
+                            Text(
+                                text = "Loading...",
+                                style = MaterialTheme.typography.titleMedium.copy(
+                                    fontWeight = FontWeight.W800,
+                                    fontSize = 16.sp,
+                                    letterSpacing = 1.1.sp
+                                ),
+                                color = colorResource(
+                                    id = R.color.text_title,
+                                )
+                            )
+                        }
+                    }
                 }
-            },
-            onFinish = {
-                onEvent(
-                    OnBoardScreenEvent.BoardEvent
-                )
             }
-        )
+            is OnBoardScreenState.IdleState -> {
+
+            }
+        }
     }
 }
 
@@ -74,7 +143,8 @@ fun OnBoardScreenPreview() {
                 )
         ) {
             OnBoardScreen(
-                onEvent = {}
+                onEvent = {},
+                state = OnBoardScreenState.LoadingState
             )
         }
     }
